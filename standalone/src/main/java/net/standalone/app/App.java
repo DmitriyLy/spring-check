5d8fbbb1cb1573bdd63cf8b9d2b346f2d5585ffa
DmitriyLy <dmitriy.lisay@gmail.com>
2021-06-29 17:58:15 +0300

    standalone

---
+++ standalone/src/main/java/net/standalone/app/App.java
@@ -0,0 +1,9 @@
+package net.standalone.app;
+
+import org.apache.commons.lang3.time.TimeZones;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println(TimeZones.GMT_ID);
+    }
+}
