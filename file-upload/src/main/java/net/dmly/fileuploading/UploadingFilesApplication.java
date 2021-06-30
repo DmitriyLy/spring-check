1380db3c22a3c249764900370c62ad08324dd3e1
DmitriyLy <dmitriy.lisay@gmail.com>
2021-06-30 19:10:13 +0300

    file-upload 1

---
+++ file-upload/src/main/java/net/dmly/fileuploading/UploadingFilesApplication.java
@@ -0,0 +1,11 @@
+package net.dmly.fileuploading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploadingFilesApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UploadingFilesApplication.class, args);
+    }
+}
---
+++ file-upload/src/main/java/net/dmly/fileuploading/controller/FileUploadController.java
@@ -0,0 +1,7 @@
+package net.dmly.fileuploading.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class FileUploadController {
+}
